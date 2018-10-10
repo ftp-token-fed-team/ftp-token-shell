c875521a79c34be3c16aaab4679e6d2a604eda5c
ousututu <xiawenjuan@ousututudeMBP.padavan>
2018-10-10 21:52:50 +0800

    update

--- platforms/android/app/src/main/java/org/apache/cordova/statusbar/StatusBar.java
+++ platforms/android/app/src/main/java/org/apache/cordova/statusbar/StatusBar.java
@@ -59,10 +59,10 @@ public class StatusBar extends CordovaPlugin {
                 Window window = cordova.getActivity().getWindow();
 
                 window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                // window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+                // window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
                 window.setStatusBarColor(Color.TRANSPARENT);
-                window.setNavigationBarColor(Color.TRANSPARENT);
+                // window.setNavigationBarColor(Color.TRANSPARENT);
 
                 window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 
